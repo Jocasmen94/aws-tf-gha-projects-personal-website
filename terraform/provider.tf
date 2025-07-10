f8ea3f4eb681b9613f3f501584687613bf9acc15
jocasmen94 <javier_jocasmen94@hotmail.com>
2025-07-10 12:40:16 -0600

    checking the workflow

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket            = "tf-resources-jocasmen"
+    bucket            = "tf-resources-jocasmen94"
     region            = "us-east-1"
     key               = "github-actions/terraform.tfstate"
     encrypt           = true
